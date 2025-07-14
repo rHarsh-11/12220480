563ab6d7557678e3c5a7cd5ffc4d9fa075d8cf8f
Harsh Rana <r.harsh.r011@gmail.com>
2025-07-14 12:01:41 +0530

    Made some changes in index.js and urlController.js

--- Backend Test Submission/index.js
+++ Backend Test Submission/index.js
@@ -14,6 +14,7 @@ app.use('/', urlRoutes);
 async function start() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB connected")
     app.listen(process.env.PORT, () =>
       console.log(`Server running on port ${process.env.PORT}`)
     );
